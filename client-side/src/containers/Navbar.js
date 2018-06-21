8600ae13352cf75b7db4a7e79dcc515bbd72c86e
viksin672 <viksin672@gmail.com>
2018-06-21 16:03:59 +0530

    client side added

---
+++ client-side/src/containers/Navbar.js
@@ -0,0 +1,109 @@
+import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { logout } from "../store/actions/auth";
+import "../style/navbar.css"
+
+class Navbar extends Component {
+  logout = e => {
+    e.preventDefault();
+    this.props.logout();
+  };
+	render(){
+		return(
+			 <div>
+              <nav class="navbar navbar-expand-lg navbar-light xo">
+    <div class="container">
+  <a class="xcv" href="/" >Qsien</a>
+   <form class="form-inline my-2 my-lg-0 lcorner">
+      <input class="form-control " type="search" placeholder="Search" aria-label="Search" />
+      </form>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item ">
+        <li class="nav-item lcon2">
+        <a class="nav-link" href="/">Home</a>
+       </li>
+      </li>
+      <li class="nav-item lcon2">
+        <a class="nav-link" href="/">Link</a>
+      </li>
+      <li class="nav-item dropdown lcon2">
+        <a class="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Notifications
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          
+        </div>
+      </li>
+      <li >
+        <a class="nav-link" href="/">Add Question</a>
+      </li>
+       </ul>
+      {this.props.currentUser.isAuthenticated ? (
+               
+            <ul className="nav navbar-nav navbar-right" >
+              <li>Hii User
+              </li>
+              <li class="nav-item lcon2">
+                <a onClick={this.logout}>Log out</a>
+              </li>
+            </ul>
+          ) : (
+            <ul className="nav navbar-nav navbar-right">
+              <li class="nav-item lcon2">
+                <Link to="/signup">Sign up</Link>
+              </li>
+              <li class="nav-item lcon2">
+                <Link to="/signin">Log in</Link>
+              </li>
+            </ul>
+          )}
+     
+   
+   
+  </div>
+
+</div>
+</nav>
+
+ 
+
+
+   
+ <div class="container sticky-top" id="hi">
+ <div class= "horizontal-scroll-wrapper  rectangles">
+  <div><a href="#home" class="ink">Education</a> </div>
+  <div> <a href="#news" class="ink">Technology</a> </div>
+  <div> <a href="#contact" class="ink">Sports</a></div>
+  <div><a href="#about" class="ink">Health</a></div>
+  <div> <a href="#about" class="ink">Dance</a></div>
+  <div> <a href="#about" class="ink">Music</a></div>
+  <div> <a href="#about" class="ink">Bollywood</a></div>
+  <div><a href="#about" class="ink">Hollywood</a></div>
+  <div><a href="#about" class="ink">Electronics</a></div>
+  <div><a href="#about" class="ink">Clothing</a></div>
+  <div><a href="#about" class="ink">Fashion</a></div>
+  <div><a href="#about" class="ink">Travel</a></div>
+  <div><a href="#about" class="ink">Politics</a></div>
+  <div><a href="#about" class="ink">Royalty</a></div>
+</div>
+</div>
+
+</div>
+
+		);
+	}
+}
+
+function mapStateToProps(state) {
+  return {
+    currentUser: state.currentUser
+  };
+}
+
+export default connect(mapStateToProps, { logout })(Navbar);
