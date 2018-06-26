12d1be78a4789a4c59bea1f4fc7bb00c137418a3
Mayank Sharma <mayanksharma250199@gmail.com>
2018-06-27 00:58:15 +0530

    Update UserDetailController.js

--- user/controller/UserDetailController.js
+++ user/controller/UserDetailController.js
@@ -87,7 +87,7 @@ router.post('/profile',upload.single('upload_file'), function(req, res) {
     });
 
     // GETS A SINGLE USER FROM THE DATABASE
-    router.get('/:id',VerifyToken, function (req, res) {
+    router.get('/:id', function (req, res) {
         User_Detail.findById(req.params.id, function (err, user) {
             if (err) return res.status(500).send("There was a problem finding the user.");
             if (!user) return res.status(404).send("No user found.");
