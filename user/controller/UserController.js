62642acd699d46175750ddbbe1d863791efd7839
Mayank Sharma <mayanksharma250199@gmail.com>
2018-06-27 01:17:37 +0530

    Update UserController.js

--- user/controller/UserController.js
+++ user/controller/UserController.js
@@ -101,7 +101,7 @@ router.get('/:id', function (req, res) {
     User.findById(req.params.id, function (err, user) {
         if (err) return res.status(500).send("There was a problem finding the user.");
         if (!user) return res.status(404).send("No user found.");
-        res.status(200).send(user);
+        res.status(200).send({id:user._id,name:user.firstname,username:user.username,email:user.email});
     });
 });
 
